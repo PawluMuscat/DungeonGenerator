ce45b6e7c766aa700664fe8b4c74a8a5c43116b9
Pawlu Muscat <pawlu.muscat@hotmail.co.uk>
2018-12-11 16:09:04 +0000

    Added Scripts to spawn the rooms based on the spawners. Spawning rooms from templates.

---
+++ Assets/Scripts/RoomSpawner.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomSpawner : MonoBehaviour
+{
+
+    public int doorDir;
+    /*
+     * #1 = Bottom
+     * #2 = Left
+     * #3 = Right
+     * #4 = Top
+     */
+
+
+    private RoomTemplates roomTemplates;
+    private int randNum;
+    private int maxRooms;
+    private int currentRooms;
+
+    void Start()
+    {
+        roomTemplates = GameObject.FindGameObjectWithTag("Rooms").GetComponent<RoomTemplates>();
+
+        //Change to a Switch Statement
+        if (doorDir == 1)
+        {
+            randNum = Random.Range(0, roomTemplates.bottomDoors.Count);
+            Instantiate(roomTemplates.bottomDoors[randNum], transform.position, roomTemplates.bottomDoors[randNum].transform.rotation);
+            //Spawn a Room with a Bottom Door.
+        }
+        if (doorDir == 2)
+        {
+            randNum = Random.Range(0, roomTemplates.leftDoors.Count);
+            Instantiate(roomTemplates.leftDoors[randNum], transform.position, roomTemplates.leftDoors[randNum].transform.rotation);
+            //Spawns a Room with a Left Door.
+        }
+        if (doorDir == 3)
+        {
+            randNum = Random.Range(0, roomTemplates.rightDoors.Count);
+            Instantiate(roomTemplates.rightDoors[randNum], transform.position, roomTemplates.rightDoors[randNum].transform.rotation);
+            //Spawn a Room with a Right Door.
+        }
+        if (doorDir == 4)
+        {
+            randNum = Random.Range(0, roomTemplates.topDoors.Count);
+            Instantiate(roomTemplates.topDoors[randNum], transform.position, roomTemplates.topDoors[randNum].transform.rotation);
+            //Spawn a Room with a Top Door.
+        }
+    }
+}
+
---
+++ Assets/Scripts/RoomTemplates.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomTemplates : MonoBehaviour {
+
+
+    public List<GameObject> leftDoors, rightDoors, topDoors, bottomDoors = new List<GameObject>();
+    public int maxRooms;
+    public int currentRooms;
+
+}
